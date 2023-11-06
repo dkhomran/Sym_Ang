b09f63e66c942dfee1c429f50bc611e59a128dd9
Dekhil Omran <dekhil22omran@gmail.com>
2023-11-06 18:25:04 +0100

    back end

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -30,21 +30,56 @@ class UserController extends AbstractController
     #[Route('/personne/store', name: 'app_personne_store', methods: ['POST'])]
     public function store(Request $request): JsonResponse
     {
-        // Récupérez les données de la requête POST
         $data = json_decode($request->getContent(), true);
-
-        // Créez une nouvelle instance de Personne
         $personne = new Personne();
         $personne->setFirstname($data['firstname']);
         $personne->setLastname($data['lastname']);
         $personne->setEmail($data['email']);
         $personne->setAge($data['age']);
+        $this->entityManager->persist($personne);
+        $this->entityManager->flush();
+        return new JsonResponse(['message' => 'Personne ajoutée avec succès'], Response::HTTP_CREATED);
+    }
 
-        // Persister l'entité dans la base de données
+    #[Route('/personne/delete/{id}', name: 'app_personne_delete')]
+    public function delete($id, PersonneRepository $personneRepository) {
+        $personne = $personneRepository->find($id);
+        if(!$personne) {
+            return new JsonResponse(['message' => 'Personne non Trouvée'], Response::HTTP_NOT_FOUND);
+        }
+        $this->entityManager->remove($personne);
+        $this->entityManager->flush();
+        return new JsonResponse(['message' => 'Personne supprimée avec succès'], Response::HTTP_OK);
+    }
+
+    #[Route('personne/edit/{id}', name: 'app_personne_edit')]
+    public function edit($id) : JsonResponse
+    {
+        $personne = $this->entityManager->getRepository(Personne::class)->find($id);
+        return $this->json($personne);
+    }
+
+    #[Route('personne/update/{id}', name: 'app_personne_update')]
+    public function update($id, Request $request)
+    {
+        $personne = $this->entityManager->getRepository(Personne::class)->find($id);
+
+        if(!$personne) {
+            return new JsonResponse(['message' => 'Personne n\'est pas trouvé']);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $personne->setFirstname($data['firstname']);
+        $personne->setLastname($data['lastname']);
+        $personne->setAge($data['age']);
+        $personne->setEmail($data['email']);
+
         $this->entityManager->persist($personne);
         $this->entityManager->flush();
 
-        // Répondez avec une réponse JSON de succès
-        return new JsonResponse(['message' => 'Personne ajoutée avec succès'], Response::HTTP_CREATED);
+
+        return new JsonResponse(['message' => 'la personne est mis a jour']);
     }
+
 }
